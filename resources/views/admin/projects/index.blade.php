3da0adae4e10fb32c78aeded3063b86000d8f975
federicomanni97 <manni_federico@libero.it>
2024-01-12 16:49:37 +0100

    create added on index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -6,5 +6,9 @@
         @foreach ($projects as $project)
         <p> <a href="{{route('admin.projects.show', $project->id)}}">{{$project->title}}</a></p>
         @endforeach
+        <form action="{{route('admin.projects.create', $project->id)}}">
+            <button class="btn btn-primary" >Create</button>
+        </form>
     </section>
+    
 @endsection
