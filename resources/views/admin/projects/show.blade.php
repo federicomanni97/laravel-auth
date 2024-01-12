427f1d49715d6b1dac555cb66a48e87984d8d9a7
federicomanni97 <manni_federico@libero.it>
2024-01-12 16:44:19 +0100

    button delete added

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,6 +4,12 @@
         <h1>{{$project->title}}</h1>
         <p>{{$project->body}}</p>
         <img src="{{$project->image}}" alt="">
-        <button class="btn btn-primary"><a href="{{route('admin.projects.edit' , $project)}}">Edit</a></button>
+        <button class="text-white"><a href="{{route('admin.projects.edit' , $project)}}">Edit</a></button>
+        <form action="{{route('admin.projects.destroy' , $project)}}" method="POST">
+            @csrf
+            @method ('DELETE')
+        <button type="submit" class="btn btn-primary">Delete</button>
+        </form>
+        
     </section>
 @endsection
