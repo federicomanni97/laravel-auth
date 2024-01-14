8793f4c45577f6f7bc920c4d8142964b6d7cebbd
federicomanni97 <manni_federico@libero.it>
2024-01-14 13:19:54 +0100

    style setup index apdated

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,30 +1,28 @@
 @extends('layouts.app')
 @section('content')
     <section class="container">
-        <h1 class="my-5">Projects</h1>
+        <h2 class="my-2 p-2">Projects</h2>
         <!-- <p>section content</p> -->
         @foreach ($projects as $project)
         <!-- <p> <a href="{{route('admin.projects.show', $project->id)}}">{{$project->title}}</a></p> -->
         <table class="table">
-        <tbody class="d-flex ">
+        <tbody class="d-flex align-middle">
             <tr>
-                <th scope="row"><a href="{{route('admin.projects.show', $project->id)}}">{{$project->title}}</a></th>
-            </tr>
-            <tr>
-                <td><a href="{{route('admin.projects.edit' , $project)}}" class="btn btn-primary">Edit</a></td>
-                <td>
+                <th scope="row"><a class="fw-normal text-decoration-none text-black" href="{{route('admin.projects.show', $project->id)}}">{{$project->title}}</a></th>
+                <th><a href="{{route('admin.projects.edit' , $project)}}" class="btn btn-primary">Edit</a></th>
+                <th>
                     <form action="{{route('admin.projects.destroy' , $project)}}" method="POST">
                     @csrf
                     @method ('DELETE')
                     <button type="submit" class="btn btn-danger">Delete</button>
                     </form>
-                </td>
+                </th>
             </tr>
         </tbody>
         </table>
         @endforeach
-        <form action="{{route('admin.projects.create', $project->id)}}">
-            <button class="btn btn-primary" >Create</button>
+        <form class="p-2" action="{{route('admin.projects.create', $project->id)}}">
+            <button class=" btn btn-primary" >Create</button>
         </form>
     </section>
 @endsection
