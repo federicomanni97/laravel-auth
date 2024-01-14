3d280cc51e2f3e0acd390d1b5726ea5e1b18eb36
federicomanni97 <manni_federico@libero.it>
2024-01-14 11:46:46 +0100

    route projects added

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,25 +2,25 @@
 
 @section('content')
 <div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+    <h1 class="text-secondary my-4">Projects</h1>
+    <!-- <div class="row justify-content-center"> -->
+        <div class="btn btn-primary">
+            <a class="text-white" href="{{route('admin.projects.index')}}">Projects</a>
+        </div>
+        <!-- <div class="col"> -->
+            <!-- <div class="card"> -->
+                <!-- <div class="card-header">{{ __('User Dashboard') }}</div> -->
 
-                <div class="card-body">
+                <!-- <div class="card-body d-flex justify-content-between align-items-center">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+                    {{ __('You are logged in!') }} -->
+                <!-- </div> -->
+            <!-- </div> -->
+        <!-- </div> -->
     </div>
 </div>
 @endsection
